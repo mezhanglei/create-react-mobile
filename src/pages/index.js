e75bf6037750a579b9f2d5cc654d15e84df285aa
mezhanglei <496623925@qq.com>
2020-07-08 00:07:15 +0800

    设置vconsole只在开发环境下加载

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 // 在多页面中需要全局引入的文件可以放在这里(非npm包引入在配置文件中配置路径)
 
-import VConsole from 'vconsole';
-if(process.env.NODE_ENV === 'development'){
-    new VConsole();
+// 只在开发环境中引入vconsole
+if (process.env.NODE_ENV === 'development') {
+    import("vconsole").then(module => {
+        const Vconsole = module.default
+        new Vconsole();
+    })
 }
 
