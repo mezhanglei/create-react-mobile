b36de22b44bd76a56e59efdd4c9ed933b6a4bdb7
mezhanglei <496623925@qq.com>
2023-02-28 18:03:54 +0800

    update

--- src/components/portal/index.tsx
+++ src/components/portal/index.tsx
@@ -3,11 +3,11 @@ import React, { CSSProperties } from 'react';
 import ReactDOM from 'react-dom';
 
 /**
- * 插入根节点组件
+ * 插入目标节点组件
  * @param props 
  * @returns 
  */
-export const BodyPortal: React.FC<{ style?: CSSProperties, customPortal?: HTMLElement }> = (props) => {
+const CreatePortal = React.forwardRef<any, { style?: CSSProperties, container?: HTMLElement, children: any }>((props, ref) => {
   const elBox = React.useRef<HTMLDivElement>(document.createElement('div'));
 
   React.useEffect(() => {
@@ -18,18 +18,14 @@ export const BodyPortal: React.FC<{ style?: CSSProperties, customPortal?: HTMLEl
     if (props?.style) {
       css(portal, props?.style);
     }
-    const root = props?.customPortal || document.body;
+    const root = props?.container || document.body;
     root.appendChild(portal);
     return () => {
       root.removeChild(portal);
     };
   }, []);
 
-  return ReactDOM.createPortal(props.children, elBox.current);
-};
+  return React.isValidElement(props.children) ? ReactDOM.createPortal(React.cloneElement(props.children, { ref }), elBox.current) : null;
+});
 
-export function withBodyPortal<P>(component: any) {
-  return React.forwardRef<unknown, P>((props, ref) => (
-    <BodyPortal>{React.createElement(component, { ...props, ref })}</BodyPortal>
-  ));
-}
+export default CreatePortal;
